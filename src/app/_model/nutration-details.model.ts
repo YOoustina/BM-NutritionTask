610b8e4e366b1d161d903918bd136101c342daa5
YoustinaNader <youstinanader5@gmail.com>
2021-12-07 09:53:43 +0200

    edit model

--- src/app/_model/nutration-details.model.ts
+++ src/app/_model/nutration-details.model.ts
@@ -1,13 +1,13 @@
 export interface NutritionDetails {
-  title: 'string';
-  ingr: ['string'];
-  url?: 'string';
-  summary?: 'string';
-  yield?: 'string';
-  time?: 'string';
-  img?: 'string';
-  prep?: 'string';
-  cuisine?: 'string';
-  mealtype?: 'string';
-  dishtype?: 'string';
+  title: string;
+  ingr: string[];
+  url?: string;
+  summary?: string;
+  yield?: string;
+  time?: string;
+  img?: string;
+  prep?: string;
+  cuisine?: string;
+  mealtype?: string;
+  dishtype?: string;
 }
